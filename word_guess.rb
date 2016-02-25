21bda68b6625a8220a107e414209494b8666d0fa
Cristal <cristalctay@gmail.com>
2016-02-25 14:28:53 -0800

    On the previous empty comment push - We put the ASCII cats into arrays so it would be easier to print the cats and take off Z's. This was done to declutter our workspace so we wouldn't have to keep putsing ASCII cats everywhere.

--- word_guess.rb
+++ word_guess.rb
@@ -1,3 +1,22 @@
+#IN-CLASS - THURS FEB 25
+#game similar to hangman - cat loses z if wrong guess
+#game over shows cat leaving
+
+# FLOW CHART
+# array of words .sample
+# picks random word from array
+# do loop to show "WORD : _ _ _ _ _ _ "
+# user input = ""
+# puts "You have already guessed: =>[letters]"
+# accept user guess input
+# evaluate user input
+  #-> is it a letter? A-Za-z (if NOT try again -loop)
+  #-> has it already been used?
+    #-> push all guessed letters in an array
+    #-> if correct letter, show "WORD: _ _ X _ _"
+    #-> if wrong letter, remove Z from sleeping cat
+
+
 class WordGuess
 
 end
@@ -5,16 +24,16 @@ end
 puts "
   Z
     Z
-      Z      |\      _,,,---,,_
+      Z      |\\     _,,,---,,_
         Z     /,`.-'`'    -.  ;-;;,_
-          Z  |,4-  ) )-,_..;\ (  `'-'
-            '---''(_/--'  `-'\_)
+          Z  |,4-  ) )-,_..;\\  ( `'-'
+            '---''(_/--'  `-'\\_)
 "
 
-puts '
+bike_cat = ['
                 ________________
                |      GAME      |_____    __
-               |      OVER!!!   |     |__|  |_________
+               |     OVER!!!    |     |__|  |_________
                |________________|     |::|  |        /
   /\**/\       |                \.____|::|__|      <
   ( o_o  )_    |                      \::/  \._______\
@@ -23,5 +42,19 @@ puts '
   ,dP"/b/=( /P"/b\
   |8 || 8\=== || 8
   `b,  ,P  `b,  ,P
-    """`     """`
-'
+    """`     """`  ']
+
+
+sleepy_cat = [
+"     Z",
+"       Z",
+"         Z",
+"           Z",
+"             Z",
+"               |\\     _,,,---,,_
+               /,`.-'`'    -.  ;-;;,_
+              |,4-  ) )-,_..;\\  ( `'-'
+             '---''(_/--'  `-'\\_)" ]
+
+puts sleepy_cat
+puts bike_cat
