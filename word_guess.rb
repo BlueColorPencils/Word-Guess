2d2dfc92e22bcefc9a0aa59a11aa9bcb9df69d2a
Cristal <cristalctay@gmail.com>
2016-02-25 15:49:33 -0800

    Moved the cat arrays to the top, so it was usable whenever. Instead of printing underscores, we made it into an array. Added the good good loop that would continue to run until a single LETTER was entered.

--- word_guess.rb
+++ word_guess.rb
@@ -2,56 +2,6 @@
 #game similar to hangman - cat loses z if wrong guess
 #game over shows cat leaving
 
-# FLOW CHART
-puts "Welcome to our game!"
-puts "Try to solve the word by guessing letters. Once the cat wakes up, the " +
-"game is over."
-
-# array of words
-words = ["tiger", "lion", "bear", "jaguar"].shuffle
-# randomly selects a word (without repeats)
-answer = words.pop
-
-# prints word: _s that represents answer
-print "word: "
-answer.length.times do
-  print "_ "
-end
-puts
-
-# prompts user for letter
-print "Please enter a letter: "
-letter = gets.chomp
-
-if letter[/[a-zA-Z]+/]  == letter
-  if letter.length == 1
-    puts "YAY"
-  else
-    puts "Please try again."
-  end
-else
-  puts "Please try again."
-end
-
-
-# picks random word from array
-# do loop to show "WORD : _ _ _ _ _ _ "
-# user input = ""
-# puts "You have already guessed: =>[letters]"
-# accept user guess input
-# evaluate user input
-  #-> is it a letter? A-Za-z (if NOT try again -loop)
-  #-> has it already been used?
-    #-> push all guessed letters in an array
-    #-> if correct letter, show "WORD: _ _ X _ _"
-    #-> if wrong letter, remove Z from sleeping cat
-#turn answer into its own array?
-
-# class WordGuess
-#   def match letter
-#     if letter
-# end
-
 bike_cat = ['
                 ________________
                |      GAME      |_____    __
@@ -77,3 +27,68 @@ sleepy_cat = [
                /,`.-'`'    -.  ;-;;,_
               |,4-  ) )-,_..;\\  ( `'-'
              '---''(_/--'  `-'\\_)" ]
+
+# FLOW CHART
+puts "Welcome to our game!"
+puts "Try to solve the word by guessing letters.
+Once the cat wakes up, the game is over."
+
+puts sleepy_cat
+
+# array of words
+words = ["tiger", "lion", "bear", "jaguar"].shuffle
+# randomly selects a word (without repeats)
+answer = words.pop
+
+# prints word: _s that represents answer
+underscores = []
+answer.length.times do
+   underscores.push("_ ")
+end
+puts
+print "word: #{underscores.join(' ')}\n"
+
+# prompts user for letter
+print "Enter a letter: "
+letter = gets.chomp
+good = ""
+guessed_letters = ["d"]
+
+# check to see if it's a letter
+until good == "good"
+  if letter[/[a-zA-Z]+/]  == letter
+    # check to see if it's ONE letter
+    if letter.length == 1
+      good = "good"
+      # places guessed letters into an array
+      guessed_letters.push(letter)
+    else
+      print "Please try again. Enter a letter: "
+      letter = gets.chomp
+    end
+  else
+    print "Please try again. Enter a letter: "
+    letter = gets.chomp
+  end
+end
+
+if answer.include? letter
+  
+end
+# picks random word from array
+# do loop to show "WORD : _ _ _ _ _ _ "
+# user input = ""
+# puts "You have already guessed: =>[letters]"
+# accept user guess input
+# evaluate user input
+  #-> is it a letter? A-Za-z (if NOT try again -loop)
+  #-> has it already been used?
+    #-> push all guessed letters in an array
+    #-> if correct letter, show "WORD: _ _ X _ _"
+    #-> if wrong letter, remove Z from sleeping cat
+#turn answer into its own array?
+
+# class WordGuess
+#   def match letter
+#     if letter
+# end
