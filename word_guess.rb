7d5be8f49c148566aa660df09f34f06e8dd621d6
Anna Wilson <annie.marie.mason@gmail.com>
2016-02-29 11:00:26 -0800

    Fixed letter_check loop so it would actually run, whichh apparently it wasn't running appropriately. Game is now functionalgit statusgit status WOWgit statusgit status Cleaned up comments.

--- word_guess.rb
+++ word_guess.rb
@@ -1,48 +1,22 @@
-#IN-CLASS - THURS FEB 25 #Look at line 62 tomorrow!
+#IN-CLASS - THURS FEB 25
 #game similar to hangman - cat loses z if wrong guess
 #game over shows cat leaving
-
-# FLOW CHART
-  # puts word.letter_check
-# words = ["loon"] #"tiger", "lion", "bear", "jaguar"].shuffle
-# randomly selects a word (without repeats)
-#
-# num = 0
-# good = ""
-# guessed_letters = [""]
-
-# prints word: _s that represents answer
-
-# puts
-
-# prompts user for letter
-
-# new_game = WordGuess.new(cat)
-  # TOMORROW ADD PROPER METHODS TO MAKE SEQUENCE OF GAME.
-  # we should write our flow chart of how we want it to run
-
-#until game is over
-  # run methods in order we think should be done
-#end
-#game over
-  #game.words_and_guessed
 
 class WordGuess
   attr_accessor :guessed_letters, :letter, :answer, :underscores, :num, :good,
   :cat_message, :sleepy_cat, :bike_cat, :words
 
   def initialize
-    @words = ["loon", "goon", "monkey", "dog"].shuffle
+    @words = ["loon", "unicorn", "baboon", "ocelot", "dachshund", "penguin", "cuttlefish",
+      "greyhound", "meerkat", "rhinoceros", "giraffe", "slug", "zonkey", "narwhal", "opossum"].shuffle
     @guessed_letters = [""]
     @letter = letter
     @answer = @words[0]
     @underscores = underscores
     @num = 0
-    @good = ""
     @cat_message = cat_message
     @sleepy_cat = sleepy_cat
     @bike_cat = bike_cat
-  #@valid_under = @answer.reverse
   end
 
   def if_statement
@@ -58,7 +32,6 @@ class WordGuess
     puts "Welcome to our game! Try to solve the word by guessing letters.
     Once sleepy cat wakes up (loses Zs), the game is over!\n\n"
     "word: #{underscores}\n"
-    "guessed letters: #{@guessed_letters.join (' ')}"
   end
 
   def cat_message
@@ -119,23 +92,21 @@ class WordGuess
 
   def letter_check
     # check to see if it's a letter
-    until @good == "good"
-      if @letter[/[a-zA-Z]+/]  == @letter
+    while true
+      if @letter[/[a-zA-Z]+/] == @letter && @letter.length == 1
         # check to see if it's ONE @letter
-        if @letter.length == 1
           if @guessed_letters.include? @letter
             try_again
+            break
           else
-            @good = "good"
             # places guessed @letters into an array
             @guessed_letters.push(@letter)
+            break
           end
         else
           try_again
+          break
         end
-      else
-        try_again
-      end
     end
   end
 
@@ -154,7 +125,6 @@ class WordGuess
         #@valid_under[0][x] = "#{@letter}"
       end
     end
-    puts @underscores.join('')
   end
 
   def drop_z
@@ -172,8 +142,6 @@ class WordGuess
   end
 
 end
-
-
 
 # entering into our game class
 game = WordGuess.new
@@ -203,27 +171,4 @@ until game.if_statement == true
   #6 if NOT part of answer, drop a Z
   game.drop_z
 
-
 end
-
-
-# right now underscores add isn't doing what it's supposed too.
-#It doesnt retain previously guessed letters.
-# it's probably something wrong with the loop
-# Last minute. we should put in a happy cat for winning
-
-# @letter_index = answer.index(letter)
-# underscores[letter_index] = letter
-# print underscores
-# picks random word from array
-# do loop to show "WORD : _ _ _ _ _ _ "
-# user input = ""
-# puts "You have already guessed: =>[letters]"
-# accept user guess input
-# evaluate user input
-  #-> is it a letter? A-Za-z (if NOT try again -loop)
-  #-> has it already been used?
-    #-> push all guessed letters in an array
-    #-> if correct letter, show "WORD: _ _ X _ _"
-    #-> if wrong letter, remove Z from sleeping cat
-#turn answer into its own array?
